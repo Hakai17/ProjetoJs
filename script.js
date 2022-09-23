50b413ee6ec3e5c62bc0c5bb7410f7e7a8410512
Hakai17 <miguel.costa.arruda08@aluno.ifce.edu.br>
2022-09-23 08:33:06 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-}
-
 //document.querySelector('.tecla_pom').onclick = tocaSomPom; (usado para botao especifico)
-
+function tocaSom (idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
- 
+/*let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    //console.log(contador);
+}*/
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
